1d48daf520057d8d4ce8a9c7376f12dca7bd7d7d
kb <kbriki@LabCab>
2016-11-26 15:28:19 -0600

    Updated algorithm

--- Python/FurtureInv.py
+++ Python/FurtureInv.py
@@ -3,18 +3,19 @@
 #!/usr/bin/python3
 
 def main():
-	print("This program calculates the furture value")
+    print("This program calculates the furture value")
 
     principal = eval(input("Enter the principal amount: "))
-          apr = eval(input("Enter the Annual Percentage Rate: "))
-     numYears = eval(input("Enter the number of Years: "))
-      userAge = eval(input("Enter your Age: "))
+    apr = eval(input("Enter the Annual Percentage Rate: "))
+    numYears = eval(input("Enter the number of Years: "))
+    userAge = eval(input("Enter your Age: "))
+    
+    apr = apr / 100
 
-
-    for i in range(10):
-        prinicpal = principal * (1 + apr)
+    for i in range(numYears):
+        total = principal * (1 + apr)
         userAge = userAge +1
 
-    print("The value in 10 years is: ", principal," and you will be:" userAge)
+    print("The value in ",numYears," years is: ", total," and you will be:", userAge)
 
 main()
