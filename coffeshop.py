38368eed3ce7fa2714161a0ddde9726370300fa9
kb <kbriki@LabCab>
2016-11-30 07:45:08 -0600

    Chapter3 lessons

---
+++ coffeshop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#
+#
+def main():
+    coffee = 10.50
+    pptax = .86
+    fixed = 1.50
+
+    cost = coffee +pptax + fixed
+
+    print("Total: ",cost)
+main()
+
--- volarea.py
+++ volarea.py
@@ -1,5 +1,5 @@
-
 #!/usr/bin/python3
+# This is  Program to calculate the volume of a Sphere
 
 import math
 def main():
