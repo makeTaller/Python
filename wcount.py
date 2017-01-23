dea4ccb0f89a702c22b4572f6f9fd5ebb31837f5
kirk tolliver <kirktolliver28@gmail.com>
2017-01-23 11:46:20 -0600

    Created a looping algorithm and a word counter

---
+++ wcount.py
@@ -0,0 +1,12 @@
+# This program counts the number of words in a sentence
+# by: Kirk Tolliver
+
+def main():
+    sentence = input("Enter the sentence: ")
+    word = sentence.split()
+
+    print(len(word))
+
+main()
+
+        
