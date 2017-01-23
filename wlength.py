3acca1ca9aff5a3e72400065de18893a454a4ef4
kirk tolliver <kirktolliver28@gmail.com>
2017-01-23 12:02:42 -0600

    Program that calculates the average word length

---
+++ wlength.py
@@ -0,0 +1,18 @@
+
+# This program counts the number of words in a sentence
+# by: Kirk Tolliver
+
+def main():
+    sentence = input("Enter the sentence: ")
+    word = sentence.split()
+
+    for w in word:
+        w = len(w)
+        avg_word = w 
+        result = avg_word + w //w
+
+    print(len(word))
+
+main()
+
+        
---
+++ wlent
@@ -0,0 +1,13 @@
+
+# This program counts the number of words in a sentence
+# by: Kirk Tolliver
+
+def main():
+    sentence = input("Enter the sentence: ")
+    word = sentence.split()
+
+    print(len(word))
+
+main()
+
+        
