0a67ccec86c1d77a8456943d85c63d04037083ee
kirk tolliver <kirktolliver28@gmail.com>
2017-01-25 16:11:48 -0600

    Word count program refactored

--- wc.py
+++ wc.py
@@ -6,25 +6,24 @@ def main():
     wc_input = input("Enter the filename: ")
 
     infile = open(wc_input,"r")
-    
-    output_words = " "
-    output_lines = [ ] 
 
-    output_char = infile.read()
-    print("Char = ",len(output_char))
+    chars = infile.read()
+    print("Characters: ",len(chars))
 
-    for word in infile.readline(): 
-        input_words = w
-        output_words = output_words + input_words
-    print("Words = ",len(output_words))
+    infile.close()
 
+
+    infile = open(wc_input,"r")
+
+    words = infile.read().split()
+    print("Words: ",len(words))
+        
+    infile.close()
+
+    infile = open(wc_input,"r")
 
     lines = infile.readlines()
-    print(len(lines))
-
-    for line in lines:
-        print("Lines = ",line)
-
+    print("Lines: ",len(lines))
     infile.close()
 
 
