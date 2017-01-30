ab162b9aedef033350f4189fbd47f12398e3aa55
Kirk Tolliver <kirktolliver28@gmail.com>
2017-01-30 13:38:01 -0600

    Syncing multiple dev enviroments

--- futval_graph.py
+++ futval_graph.py
@@ -1,48 +1,52 @@
-# futval_graph.py
-# Draw scale labels on left side of window
-#
-# Draw label " 0.0K " at (20,230)
-# Draw lable " 2.5K"  at (20,180)
-# Draw lable " 5.0K"  at (20,130)
-# Draw lable " 7.5K " at (20,80)
-# Draw label "10.0K"  at (20,30)
+'''
+futval_graph.py
+
+
+Created on Dec 14, 2016
+
+@author: dsparrow
+'''
+
 from graphics import *
 
 def main():
-    #Introduction
-    print("This program plots the growth of a 10-year investment.")
-
-    #Get principal and interest rate
-    principal = eval(raw_input("Enter the initial principal: "))
-    apr = eval(raw_input("Enter the annualized interest rate: "))
-    # Get a graphics window with labels on left edge
-    win = GraphWin("Investment Growth Chart", 420,240)
+    # Indroduction
+    
+    print("This program plots the  growth of a 10-year investment.")
+    
+    
+    # Get principal and interest rate
+    principal = eval(raw_input("Enter the initial  principal: "))
+    apr = eval(raw_input("Enter the annualized interest rate:"))
+    
+    # Create a graphic window with labels on left 
+    win = GraphWin("Investment Growth Chart", 320, 240)
+    win.setCoords(-1.75, -200, 11.5, 10400)
     win.setBackground("white")
-    win.setCoords(-1.75,-200,11.5,10400)
-    Text(Point(-1,0), ' 0.0K').draw(win)
-    Text(Point(-1,2500), ' 2.5K').draw(win)
-    Text(Point(-1,5000), ' 5.0K').draw(win)
-    Text(Point(-1,7500), ' 7.5K').draw(win)
-    Text(Point(-1,10000), ' 10.0K').draw(win)
-
+    Text(Point(-1,0), '0.0K').draw(win)
+    Text(Point(-1,2500), '2.5K').draw(win)
+    Text(Point(-1,5000), '5.0K').draw(win)
+    Text(Point(-1,7500), '7.5K').draw(win)
+    Text(Point(-1,10000), '10.0K').draw(win)
+    
     # Draw bar for initial principal
-    #height = principal * 0.02
-    bar = Rectangle(Point(0,0), Point(1,principal))
+    bar = Rectangle(Point(0, 0), Point(1,principal))
     bar.setFill("green")
     bar.setWidth(2)
     bar.draw(win)
 
-    # Draw bars for sucessive years
+    # Draw bars for successive years
     for year in range(1,11):
-        #calculate value for the next years
-        principal = principal * (1 + apr)
+        # calculate value for the years
+        principal  = principal * (1 + apr)
         #draw bar for this value
-        bar = Rectangle(Point(year,0), Point(year +1, principal))
+        bar = Rectangle(Point(year,0), Point(year + 1, principal))
         bar.setFill("green")
         bar.setWidth(2)
         bar.draw(win)
-
-    raw_input("Press <Enter> to quit")
+        
+        
+    input("Press <Enter> to quit")
+    win.getMouse()
     win.close()
-
 main()
